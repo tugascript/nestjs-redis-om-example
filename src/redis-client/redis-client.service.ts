a8cce0182043f8b8f762c11921342477d291ac48
Afonso Barracha <barracha.afonso@gmail.com>
2022-08-24 19:49:05 +0100

    Add On Module Destroy

--- src/redis-client/redis-client.service.ts
+++ src/redis-client/redis-client.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Client } from 'redis-om';
 
 @Injectable()
-export class RedisClientService extends Client {
+export class RedisClientService extends Client implements OnModuleDestroy {
   constructor(private readonly configService: ConfigService) {
     super();
     (async () => {
       await this.open(configService.get<string>('redisUrl'));
     })();
+  }
+
+  public async onModuleDestroy() {
+    await this.close();
   }
 }
